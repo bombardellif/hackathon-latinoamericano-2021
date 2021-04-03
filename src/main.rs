7ad9134bf500b4f063ca6c02d20f56b5ddfbfa9d
Fernando Bombardelli da Silva <bombardelli.f@mailbox.tu-berlin.de>
2021-04-02 21:44:37 -0300

    Work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::prelude::*;
-use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+use chrono::offset;
+
 
 #[derive(Debug)]
 struct UserMessage {
@@ -9,24 +13,45 @@ struct UserMessage {
     intent: String,
 }
 
-trait Action<'a> {
-    fn run(&self, responses: &HashMap<&'a str, &'a str>) -> &'a str;
+#[derive(Debug)]
+enum Action<'a> {
+    UtterMessage(&'a str),
+    CurrentTime,
+    PlainTextSlotSet,
 }
 
 #[derive(Debug)]
-struct UtterMessage<'a> {
-    utterance_name: &'a str
-}
-impl<'a> Action<'a> for UtterMessage<'_> {
-    fn run(&self, responses: &HashMap<&'a str, &'a str>) -> &'a str {
-        responses.get(self.utterance_name).unwrap_or(&"")
-    }
+enum Event<'responses, 'slots> {
+    BotUtteredTemplate(&'responses str),
+    BotUtteredText(String),
+    SlotSet(&'slots str, Option<String>),
 }
 
-struct ActionCurrentTime {}
-impl<'a> Action<'a> for ActionCurrentTime {
-    fn run(&self, _: &HashMap<&'a str, &'a str>) -> &'a str {
-        "18:00"
+
+impl<'responses, 'slots> Action<'responses> {
+    async fn run(
+        &self,
+        user_message: &UserMessage,
+    ) -> Vec<Event<'responses, 'slots>> {
+        match self {
+            Action::UtterMessage(tpl_name) => 
+                vec![
+                    Event::BotUtteredTemplate(tpl_name),
+                ],
+            Action::CurrentTime => 
+                vec![
+                    Event::BotUtteredText(
+                        offset::Local::now().time()
+                            .format("%H:%M:%S")
+                            .to_string()),
+                ],
+            Action::PlainTextSlotSet => 
+                vec![
+                    Event::SlotSet(
+                        "plain_text",
+                        Some(user_message.text.clone())),
+                ],
+        }
     }
 }
 
@@ -39,31 +64,72 @@ fn parse_input(input: &str) -> UserMessage {
     }
 }
 
-fn main() {
+
+async fn process_messsage<'slots, 'responses>(
+    responses: Arc<HashMap<&'responses str, &'responses str>>,
+    rules: Arc<HashMap<&'responses str, Action<'responses>>>,
+    slots: Arc<Mutex<HashMap<&'slots str, Option<String>>>>,
+    input: &str,
+) {
+    let fallback_rule = Action::UtterMessage("default_fallback");
+
+    let user_message = parse_input(input);
+
+    let action = rules.get(&user_message.intent as &str)
+        .unwrap_or(&fallback_rule);
+
+    for event in action.run(&user_message).await {
+        match event {
+            Event::BotUtteredTemplate(tpl_name) =>
+                println!("{}", responses.get(tpl_name).unwrap()),
+            Event::BotUtteredText(text) =>
+                println!("{}", text),
+            Event::SlotSet(key, value) => {
+                slots.lock().unwrap().insert(key, value);
+                println!("{:?}", slots)
+            },
+        }
+    }
+}
+
+
+#[tokio::main]
+async fn main() {
     let responses: HashMap<_, _> = vec![
         ("utter_hello", "Oi!"),
+        ("utter_bye", "Tchau!"),
         ("default_fallback", "Ops, não te entendi"),
     ].into_iter()
      .collect();
+    let arc_responses = Arc::new(responses);
 
-    let rules: HashMap<_, &dyn Action> = vec![
-        ("oi", &UtterMessage{utterance_name: "utter_hello"} as &dyn Action),
-        ("horas", &ActionCurrentTime{} as &dyn Action),
+    let rules: HashMap<_, _> = vec![
+        ("oi", Action::UtterMessage("utter_hello")),
+        ("tchau", Action::UtterMessage("utter_bye")),
+        ("horas", Action::CurrentTime),
+        ("texto livre", Action::PlainTextSlotSet),
     ].into_iter()
      .collect();
+    let arc_rules = Arc::new(rules);
 
-    let fallback_rule: &dyn Action =
-        &UtterMessage{utterance_name: "default_fallback"};
+    let slots: HashMap<_, Option<String>> = vec![
+        ("plain_text", None),
+    ].into_iter()
+     .collect();
+    let arc_slots = Arc::new(Mutex::new(slots));
 
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
-        let user_message = parse_input(&line.unwrap());
-
-        let action = rules.get(&user_message.intent as &str)
-            .unwrap_or(&fallback_rule);
-
-        let utterance = action.run(&responses);
-
-        println!("{}", utterance);
+        let arc_responses_clone = arc_responses.clone();
+        let arc_rules_clone = arc_rules.clone();
+        let arc_slots_clone = arc_slots.clone();
+        tokio::spawn(async move {
+            process_messsage(
+                arc_responses_clone,
+                arc_rules_clone,
+                arc_slots_clone,
+                &line.unwrap())
+                .await
+        });
     }
 }
